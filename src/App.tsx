e1af516babddd1c0c8142363eff11e142c91b3d5
Adrián Naranjo <adriannaranjosanchez@gmail.com>
2021-12-30 12:07:16 +0100

    firebase added to project

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,8 @@
+import { initializeApp } from "@firebase/app";
+import { firebaseConfig } from "./firebase/config";
 import { AppRouter } from "./routers/AppRouter";
+
+initializeApp(firebaseConfig);
 
 const App: React.FC = () => {
   return (
